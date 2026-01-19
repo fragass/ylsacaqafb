8bc0756ba6bc07ded5c9d4bbf16b3e430987caaa
fragass <fkp9wx6y@gmail.com>
2026-01-19 11:36:10 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const animeCapa = "imgs/spyxfamily.webp";
+const animeCapa2 = "imgs/anotheranime.webp";
 
 const catalogo = document.getElementById("catalogo");
 
@@ -17,6 +18,21 @@ catalogo.appendChild(card1);
 // redirecionamento ao clicar
 card1.addEventListener("click", () => {
   window.location.href = "oixdtwfvgt.html";
+});
+// ===== CARD 2 =====
+const card2 = document.createElement("div");
+card2.className = "card";
+card2.id = "card2";
+
+const img2 = document.createElement("img");
+img2.src = animeCapa2;
+img2.loading = "lazy";
+
+card2.appendChild(img2);
+catalogo.appendChild(card2);
+
+card2.addEventListener("click", () => {
+  window.location.href = "outrapagina.html";
 });
 
 
