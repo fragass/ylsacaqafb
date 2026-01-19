8c516c4ed17ae9b3b18d6c3213b7d6d24df20615
fragass <fkp9wx6y@gmail.com>
2026-01-19 11:32:29 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,24 +18,26 @@ catalogo.appendChild(card1);
 card1.addEventListener("click", () => {
   window.location.href = "oixdtwfvgt.html";
 });
-const animeCapa2 = "imgs/spyxfamily.webp";
+const animeCapa = "imgs/spyxfamily.webp";
 
 const catalogo = document.getElementById("catalogo");
 
 // card1
-const card2 = document.createElement("div");
-card2.className = "card";
-card2.id = "card2";
+const card1 = document.createElement("div");
+card1.className = "card";
+card1.id = "card1";
 
 const img = document.createElement("img");
-img.src = animeCapa2;
+img.src = animeCapa;
 img.loading = "lazy";
 
-card2.appendChild(img);
+card1.appendChild(img);
 catalogo.appendChild(card1);
 
 // redirecionamento ao clicar
-card2.addEventListener("click", () => {
+card1.addEventListener("click", () => {
   window.location.href = "oixdtwfvgt.html";
 });
+
+
 
